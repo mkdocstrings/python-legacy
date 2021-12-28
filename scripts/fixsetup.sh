e28cee7c472feabaeca857f76aea6e8b8c8620cf
Timothée Mazzucotelli <pawamoy@pm.me>
2021-12-28 19:14:10 +0100

    chore: Add Python 3.7 to helper script

--- scripts/fixsetup.sh
+++ scripts/fixsetup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-PYTHON_VERSIONS="${PYTHON_VERSIONS-3.8 3.9 3.10 3.11}"
+PYTHON_VERSIONS="${PYTHON_VERSIONS-3.7 3.8 3.9 3.10 3.11}"
 
 for python_version in ${PYTHON_VERSIONS}; do
     rm -rf "__pypackages__/${python_version}/lib/mkdocstrings"
