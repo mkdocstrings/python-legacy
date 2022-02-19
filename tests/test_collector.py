8e9966c2b764ba52e1ecf0b79c69662245dd9565
Rachmadani Haryono <foreturiga@gmail.com>
2022-02-19 21:18:06 +0800

    test: test_collector

---
+++ tests/test_collector.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pytest
+
+from mkdocstrings.handlers.base import CollectionError
+from mkdocstrings.handlers.python import collector
+
+
+def test_init():
+    collector.PythonCollector()
+
+
+def test_collect():
+    obj = collector.PythonCollector()
+    with pytest.raises(CollectionError):
+        obj.collect("", {})
